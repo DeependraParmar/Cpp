bb61c82659e77e4954a6cd30f3d5a194debbd62f
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2022-10-23 21:45:34 +0530

    Add files via upload

---
+++ 02_variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // variables are the container or technically are the memory allocations or blocks which are used to hold a praticular type of data    
+    int a = 5;//integer variable
+    float f = 9.09;//float variable
+    char c = 'c';// character variable
+
+    cout<<"Integer is: "<<a<<endl;
+    cout<<"Float is: "<<f<<endl;
+    cout<<"Character is: "<<c<<endl;
+
+
+    return 0;
+}
