2e8a5b6fffe9d4b15a9cdcf0ef8207daa4b1ac4a
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2022-10-23 21:45:55 +0530

    Add files via upload

---
+++ 03_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Here we are going to take input from the user .
+    int a;
+    cout<<"Enter the value of variable a: ";
+    cin>>a;
+
+    cout<<endl<<"a is: "<<a<<endl;
+    return 0;
+}
